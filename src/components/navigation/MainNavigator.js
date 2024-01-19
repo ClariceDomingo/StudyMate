a517e3675a208fc67e310a33b47abba283e1c587
Aubrey Heart A <arian.aubreyheart18@gmail.com>
2024-01-19 21:41:46 +0800

    Settings: partial

--- src/components/navigation/MainNavigator.js
+++ src/components/navigation/MainNavigator.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Icon } from "react-native-paper";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import TaskScreen from "../screens/Task/TaskScreen";
 import TimerScreen from "../screens/Timer/TimerScreen";
 import ProfileScreen from "../screens/Profile/ProfileScreen";
 import SettingsScreen from "../screens/Settings/SettingsScreen";
+
 import LoginForm from "../../components/forms/LoginForm";
 import ForgotPasswordScreen from "../forms/ForgotPasswordForm";
 import SignUpForm from "../forms/SignUpForm"; 
 
-const Tab = createBottomTabNavigator();
+import Help from "../screens/Settings/Help";
+import About from "../screens/Settings/About";
 
-export default function MainNavigator() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function HomeTabs() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -20,54 +27,37 @@ export default function MainNavigator() {
   }, []);
 
   return (
-    <Tab.Navigator screenOptions={{ tabBarStyle: { height: 55 } }} initialRouteName="Profile">
-      {/* Profile Screen */}
-      <Tab.Screen
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="account" size={30} />
-          ),
-        }}
-        name="Profile"
-        component={ProfileScreen}
-      />
+    <Tab.Navigator
+      initialRouteName="Settings"
+      screenOptions={({ route }) => ({        
+        headerShown: false,
+        tabBarStyle: { height: 55, }, 
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-      {/* Task Screen */}
-      <Tab.Screen
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="notebook" size={30} />
-          ),
-        }}
-        name="Task"
-        component={TaskScreen}
-      />
+          // Set the icon name based on the route
+          if (route.name === 'Profile') {
+            iconName = focused ? 'account' : 'account-outline';
+          } else if (route.name === 'Task') {
+            iconName = focused ? 'notebook' : 'notebook-outline';
+          } else if (route.name === 'Timer') {
+            iconName = focused ? 'timer' : 'timer-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'cog' : 'cog-outline';
+          }
 
-      {/* Timer Screen */}
-      <Tab.Screen
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="timer" size={30} />
-          ),
-        }}
-        name="StudyTimer"
-        component={TimerScreen}
-      />
-
-      {/* Settings Screen */}
-      <Tab.Screen
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Icon source="settings-helper" size={30} />
-          ),
-        }}
-        name="Settings"
-        component={SettingsScreen}
-      />
+          // You can return any component here
+          return <Icon source={iconName} size={30} color={color} />;
+        },
+        
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >  
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Task" component={TaskScreen} />
+      <Tab.Screen name="Timer" component={TimerScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} options={{ headerShown: true }} />
 
       {}
       {isLoggedIn ? null : (
@@ -102,5 +92,15 @@ export default function MainNavigator() {
   />
 ) : null}
     </Tab.Navigator>
+  );
+}
+
+export default function MainNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='tab' component={HomeTabs} />
+      <Stack.Screen name='Help' component={Help} />
+      <Stack.Screen name='About' component={About} />
+    </Stack.Navigator>
   );
 }
