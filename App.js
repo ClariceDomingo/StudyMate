351499a5f9b9dfa98199e999df6df1f2dff04ce4
Aubrey Heart A <arian.aubreyheart18@gmail.com>
2024-01-19 18:33:11 +0800

    Settings: Partial

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+
+import MainNavigator from './src/components/navigation/MainNavigator';
+import LandingPage from "./src/components/forms/LandingPage";
+
+export default function App() {
+    const [showLandingPage, setShowLandingPage] = useState(true);
+
+  const handleStart = () => {
+    setShowLandingPage(false);
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+
+        <NavigationContainer>
+          {showLandingPage ? (
+            <LandingPage onStart={handleStart} />
+          ) : (
+            <MainNavigator />
+          )}
+        </NavigationContainer>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
