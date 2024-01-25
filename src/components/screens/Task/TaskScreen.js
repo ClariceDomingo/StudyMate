5e6ea7814845753f137c989b4ece4eae2f85678a
Clarice Domingo <domingo.clarice7@gmail.com>
2024-01-26 01:44:15 +0800

    Task: AsyncStorage

--- src/components/screens/Task/TaskScreen.js
+++ src/components/screens/Task/TaskScreen.js
@@ -4,6 +4,7 @@ import TaskList from "../../forms/TaskList";
 import TaskModal from "../../forms/TaskModal";
 import styles from "../../../config/TaskStyles";
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const backgroundImage = require("../../../../assets/emptyImage.png");
 
@@ -33,6 +34,42 @@ const TaskScreen = ({ route }) => {
       setTasks(route.params.updatedTasks);
     }
   }, [route.params?.updatedTasks]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const savedTasks = await AsyncStorage.getItem('tasks');
+        const savedCategories = await AsyncStorage.getItem('categories');
+
+        if (savedTasks) {
+          setTasks(JSON.parse(savedTasks));
+          setFilteredTasks(JSON.parse(savedTasks));
+        }
+
+        if (savedCategories) {
+          setCategories(JSON.parse(savedCategories));
+        }
+      } catch (error) {
+        console.error('Error loading data:', error);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  // Save tasks and categories to AsyncStorage whenever they change
+  useEffect(() => {
+    const saveData = async () => {
+      try {
+        await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+        await AsyncStorage.setItem('categories', JSON.stringify(categories));
+      } catch (error) {
+        console.error('Error saving data:', error);
+      }
+    };
+
+    saveData();
+  }, [tasks, categories]);
 
   const handleAddTask = () => {
     setEditingTask(null);
@@ -118,6 +155,7 @@ const TaskScreen = ({ route }) => {
     } else {
       setValidationError(true);
     }
+    saveData();
   };
   
 
@@ -176,6 +214,16 @@ const TaskScreen = ({ route }) => {
       handleFilterByCategory("All");
     } else {
       setFilteredTasks(tasks.filter((t) => t.category.toLowerCase() === selectedCategory.toLowerCase()));
+    }
+    saveData();
+  };
+
+  const saveData = async () => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+      await AsyncStorage.setItem('categories', JSON.stringify(categories));
+    } catch (error) {
+      console.error('Error saving data to AsyncStorage:', error);
     }
   };
 
