0f8be4ab9890c01a6f0499a94fa20768e370a19e
Clarice Domingo <domingo.clarice7@gmail.com>
2024-01-27 11:17:45 +0800

    ...

--- src/components/screens/Settings/SettingsScreen.js
+++ src/components/screens/Settings/SettingsScreen.js
@@ -65,7 +65,7 @@ export default function SettingsScreen() {
   const shareApp = async () => {
     try {
       const result = await Share.share({
-        message: 'Check out this app on the Play Store: https://play.google.com/store/apps/details?id=com.netmarble.tog&pcampaignid=web_share',
+        message: 'Check out this app on the Play Store: https://expo.dev/accounts/claricedomingo/projects/StudyMate/builds',
         title: 'Share App',
         url: 'https://expo.dev/accounts/claricedomingo/projects/StudyMate/builds',
       });
